4eac2842f36889110604592e35ca3d331c1f1096
Mazharul <mazharul.islam@durbar.live>
2024-06-08 16:40:17 +0600

    vue layout setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/js/bootstrap.js'
 
 import App from './App.vue'
 import router from './router'
