09e0e68ea6d09f554924f56cfd40163e94bd508b
Mazharul <mazharul.islam@durbar.live>
2024-06-09 13:04:40 +0600

    login page design

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-5 w-50 mx-auto">
+    <div class="card p-3">
+      <h5 class="mb-3">Login</h5>
+      <div class="">
+        <input type="email" class="form-control" placeholder="Email">
+      </div>
+      <div class="mt-3">
+        <input type="password" class="form-control" placeholder="password">
+      </div>
+      <div class="mt-3">
+        <button class="btn btn-success">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
